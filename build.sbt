9c85f7a3f1e1d1faf4e692a0d3464dbee54cfd77
sureshchandra.emb@gmail.com <sureshchandra.emb@gmail.com>
2018-02-24 18:09:14 +0700

    commit all files

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "SparkDataFrame"
+
+version := "0.1"
+
+scalaVersion := "2.11.8"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-sql
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.2.0"
---
+++ src/main/scala/com.course.spark.dataframe/DateAndTime.scala
@@ -0,0 +1,33 @@
+package com.course.spark.dataframe
+import org.apache.spark.sql.functions._
+object DateAndTime {
+  // Dates and TimeStamps
+
+  // Start a simple Spark Session
+  import org.apache.spark.sql.SparkSession
+  val spark = SparkSession.builder().getOrCreate()
+
+  // Create a DataFrame from Spark Session read csv
+  // Technically known as class Dataset
+  val df = spark.read.option("header","true").option("inferSchema","true").csv("CitiGroup2006_2008")
+
+  // Show Schema
+  df.printSchema()
+
+  // Lot's of options here
+  // http://spark.apache.org/docs/latest/api/scala/index.html#org.apache.spark.sql.functions$@add_months(startDate:org.apache.spark.sql.Column,numMonths:Int):org.apache.spark.sql.Column
+
+  df.select(month(df("Date"))).show()
+
+  df.select(year(df("Date"))).show()
+
+  // Practical Example
+  val df2 = df.withColumn("Year",year(df("Date")))
+
+  // Mean per Year, notice large 2008 drop!
+  val dfavgs = df2.groupBy("Year").mean()
+
+  //Todo :: verify if below code is correct, as it was like select($"Year","$avg(Close)" before
+  dfavgs.select("Year","avg(Close)").show()
+
+}
